d128d1fde42b07bb48a7c229bc2903aeaa1affbb
Sumit Mourya <mouryasumit0@gmail.com>
2023-06-09 10:56:12 +0530

    updated HomeScreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-
+import Homescreen from '@/components/Homescreen'
+import Nav from '@/components/Nav'
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello Tvastte Motors!
-  </h1>
+  <>
+    <Nav/>
+    <Homescreen />
+  </>
   )
 }
---
+++ src/components/Homescreen.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import localFont from 'next/font/local'
+ 
+// Font files can be colocated inside of `app`
+const myFont = localFont({
+  src: '../../public/fonts/space-age.ttf',
+  display: 'swap',
+})
+
+const Homescreen = () => {
+  return (
+    <>   
+    <div className="fixed h-screen w-screen overflow-hidden -z-10">
+    <Image
+      alt="JCB"
+      src='/images/jcb.jpg'
+      placeholder="blur"
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8doi1HgAGsQI+jZszvgAAAABJRU5ErkJggg=="
+      quality={100}
+      fill
+      sizes="100vw"
+      style={{
+        objectFit: 'cover',
+      }}
+    />
+  </div>
+  <div className={`${myFont.className} w-96 ml-72 pt-72` }>
+  <h1 className="text-6xl mb-0">
+      <span className="text-yellow-400">Earth</span>
+      <br />
+      <span className="text-white">Shaping Solution</span>
+  </h1>
+    <hr className="w-96 h-1 bg-yellow-400 border-0 rounded md:my-2"/>
+  </div>
+
+  </>
+  )
+}
+
+export default Homescreen;
